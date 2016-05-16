5ace14b9a73dbaaef3749d1aedaa713e3e5134dd
Benjamin M <benjamarle@gmail.com>
2016-05-15 22:05:45 -0400

    Immersive mode

--- src/net/zorgblub/typhon/fragment/ReadingFragment.java
+++ src/net/zorgblub/typhon/fragment/ReadingFragment.java
@@ -1229,9 +1229,13 @@ public class ReadingFragment extends Fragment implements
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             if (config.isFullScreenEnabled()) {
-                bookView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                activity.getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
             }
             if (config.isDimSystemUI()) {
